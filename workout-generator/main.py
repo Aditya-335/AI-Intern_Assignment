11767bddb2a96f5f195ab948bfdb38b4338befb1
Aditya Borhade <143924501+Aditya-335@users.noreply.github.com>
2025-05-10 13:28:40 +0530

    Data being displayed successfully on frontend

--- workout-generator/main.py
+++ workout-generator/main.py
@@ -2,8 +2,16 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 from workout_logic import generate_workout_plan
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class UserProfile(BaseModel):
     name: str
@@ -19,5 +27,5 @@ def create_plan(profile: UserProfile):
     return {
         "name": profile.name,
         "goal": profile.goal,
-        "plan": generate_workout_plan(profile.dict())
+        "plan": generate_workout_plan(profile.model_dump())
     }

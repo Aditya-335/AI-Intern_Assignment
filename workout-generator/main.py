96ab628d4a7dc22982c55f6bd1db6c1b7ca9c1dd
Aditya Borhade <143924501+Aditya-335@users.noreply.github.com>
2025-06-08 11:13:25 +0530

    CORS issue resolution

--- workout-generator/main.py
+++ workout-generator/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"], 
+    allow_origins=["https://ai-intern-assignment.onrender.com"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
